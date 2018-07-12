b9789a4f681b5eac7916803ed995dba74732421b
DumberDuDe <nicolas.paris@socgen.com>
2018-07-12 14:04:21 +0200

    use postgresql db instead of the dict

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,24 @@
 # wsgi.py
-from flask import Flask, jsonify, abort, make_response, request
-app = Flask(__name__)
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except:
+    pass # Heroku does not use .env
 
-THE_PRODUCTS = [
-    {'id': 1, 'name': 'Skello'},
-    {'id': 2, 'name': 'Socialive.tv'},
-    {'id': 3, 'name': 'foo.tv'},
-    {'id': 4, 'name': 'bar.audio'},
-    {'id': 5, 'name': 'foobar'},
-    ]
+from flask import Flask, jsonify, abort, make_response, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+# models requires the 'db' object
+from models import Product
+# schemas requires the 'ma' object
+from schemas import product_schema, products_schema
 
 @app.route('/')
 def hello():
@@ -16,31 +26,33 @@ def hello():
 
 @app.route('/api/v1/products')
 def list_products():
-    return jsonify(THE_PRODUCTS)
+    products = db.session.query(Product).all()
+    return products_schema.jsonify(products)
 
 @app.route('/api/v1/products/<int:id>', methods=['GET'])
 def get_a_product(id):
-    id_to_return = [x for x in THE_PRODUCTS if x['id'] == id]
-    return jsonify(id_to_return[0]) if id_to_return != [] else abort(404)
+    product = db.session.query(Product).get(id)
+    return product_schema.jsonify(product) if product else abort(404)
 
 @app.route('/api/v1/products/<int:id>', methods=['DELETE'])
 def del_a_product(id):
-    global THE_PRODUCTS
-    if not get_a_product(id).status_code == 200:
+    product = db.session.query(Product).get(id)
+    if product:
+        db.session.delete(product)
+        db.session.commit()
+        return make_response(("Product has been deleted!", 204))
+    else:
         abort(404)
-    THE_PRODUCTS = [x for x in THE_PRODUCTS if x['id'] != id]
-    return make_response(("Product has been deleted!", 204))
 
 @app.route('/api/v1/products', methods=['POST'])
 def create_a_product():
     if not request.is_json:
         return make_response(f"No JSON found", 400)
-    THE_PRODUCTS.append(
-        {
-            'id': len(THE_PRODUCTS) + 1,
-            'name': request.json['name'],
-        })
-    return make_response(jsonify({'id': len(THE_PRODUCTS)}), 201)
+    product = Product()
+    product.name = request.json['name']
+    db.session.add(product)
+    db.session.commit()
+    return make_response(("Product has been added", 201))
 
 @app.route('/api/v1/products/<int:id>', methods=['PATCH'])
 def update_a_product(id):
@@ -52,9 +64,9 @@ def update_a_product(id):
         return make_response(f"No 'name' entry found in the JSON", 422)
     elif request.json['name'] == "":
         return make_response(f"The 'name' entry must not be empty", 422)
-    if not get_a_product(id).json['id'] == id:
+    product = db.session.query(Product).get(id)
+    if not product:
         return make_response(f"ID {id} not found", 400)
-    for product in [x for x in THE_PRODUCTS if x['id'] == id]:
-        product['name'] = request.json['name']
-        break
+    product.name = request.json['name']
+    db.session.commit()
     return make_response(get_a_product(id), 201)
